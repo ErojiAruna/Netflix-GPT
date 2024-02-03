91fc52a15fb48201400de64c2a288cc29cca0cac
ErojiAruna <aruna.eroji@gamil.com>
2024-02-03 19:44:31 +0530

    openai

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPENAI_API_KEY } from './constants';
 
 const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY || 'My API Key',
   dangerouslyAllowBrowser: true,
 });
 
