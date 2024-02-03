dcf0f647d210cd6327dc8f10bcca3de1c9d71a2e
ErojiAruna <aruna.eroji@gamil.com>
2024-02-03 13:01:50 +0530

    API key changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,4 +23,4 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: 'spanish', name: 'Spanish' },
 ];
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_API_KEY;
